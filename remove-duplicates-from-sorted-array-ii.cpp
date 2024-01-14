7dd6782d4a605feefdd6e22b7fa057b32c0747ef
techJaisingh <156594315+techJaisingh@users.noreply.github.com>
2024-01-14 20:31:02 +0530

    Create remove-duplicates-from-sorted-array-ii.cpp
    Leetcode

---
+++ remove-duplicates-from-sorted-array-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+
+        int i = 0; 
+        for(auto num : nums){
+
+            if(i < 2 || nums[i-2] < num){
+
+                nums[i] = num;
+                i++;
+            }
+        }
+        
+        return i;
+        
+    }
+};
